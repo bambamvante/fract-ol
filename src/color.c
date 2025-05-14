6ffb529a662accfc126d04cbeeeef4d60b7ef62a
Arocha Phuengthai <arphueng@z3t11c4.42bangkok.com>
2025-05-14 13:08:12 +0700

    d

--- src/color.c
+++ src/color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baebae <baebae@student.42.fr>              +#+  +:+       +#+        */
+/*   By: arphueng <arphueng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 15:46:41 by arphueng          #+#    #+#             */
-/*   Updated: 2025/05/10 17:49:23 by baebae           ###   ########.fr       */
+/*   Updated: 2025/05/14 13:07:56 by arphueng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,14 @@ int	set_color(t_fractol *f, int iter)
 	unsigned int	r;
 	unsigned int	g;
 	unsigned int	b;
-	// double			s;
 
-	// s = (double)iter / f->max_iter;
 	if (iter == f->max_iter)
 		color = create_color(0, 0, 0);
 	else
 	{
 		r = (iter * 0) % 256;
-        g = (iter * 30) % 256;
-        b = (iter * 0) % 256;
-		// r = (unsigned int)(9 * (1 - s) * s * s * s * 255);
-		// g = (unsigned int)(15 * (1 - s) * (1 - s) * s * s * 255);
-		// b = (unsigned int)(8.5 * (1 - s) * (1 - s) * (1 - s) * s * 255);
+		g = (iter * 30) % 256;
+		b = (iter * 0) % 256;
 		color = create_color(r, g, b);
 	}
 	return (color);
